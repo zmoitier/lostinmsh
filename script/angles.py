a91b56c715aaefbc26e6b1f8aa41b07404820fa8
zmoitier <z.moitier@gmail.com>
2026-01-19 16:52:04 +0100

    small change

--- script/angles.py
+++ script/angles.py
@@ -1,13 +1,15 @@
 from fractions import Fraction
 
-import matplotlib.pyplot as plt
 import numpy as np
 
 
-def compute_pq(angle: float, max_denominator: int) -> tuple[int, int]:
+def compute_pq_frac(angle: float, max_denominator: int) -> tuple[int, int]:
     """Compute p and q for a given angle."""
 
     r = Fraction(angle / np.pi).limit_denominator(max_denominator)
+    if r.numerator == 0:
+        r = Fraction(1, max_denominator)
+
     s = Fraction(r.numerator, 2 * r.denominator - r.numerator)
     p, q = s.numerator, s.denominator
     if ((p + q) % 2 == 1) or (p == 1) or (q == 1):
@@ -17,23 +19,44 @@ def compute_pq(angle: float, max_denominator: int) -> tuple[int, int]:
     return (p, q)
 
 
-def main(max_dem: int) -> None:
-    rat_set = set([Fraction(0, 1)])
-    for n in range(1, max_dem + 1):
-        for k in range(1, 2 * n):
-            rat = Fraction(k, n)
-            rat_set.add(rat)
+def compute_pq_min(angle: float, max_subdiv: int) -> tuple[int, int]:
+    """Compute p and q for a given angle."""
 
-    print(len(rat_set))
+    r = (2 * np.pi - angle) / angle
 
-    angles = np.pi * np.array(sorted(rat_set), dtype=np.float64)
+    p_min, q_min = 0, 0
+    _min = np.abs(np.log(r))
+    for n in range(4, max_subdiv + 1, 2):
+        p, q = np.arange(2, n - 1), np.arange(n - 2, 1, -1)
+        v = np.abs(np.log(r * p / q))
 
-    fig, ax = plt.subplots()
-    ax.set_aspect("equal")
-    ax.grid()
-    ax.plot(np.cos(angles), np.sin(angles), "o")
-    plt.show()
+        i = v.argmin()
+        if v[i] < _min:
+            p_min, q_min = p[i], q[i]
+            _min = v[i]
+
+        if _min < 1e-12:
+            break
+
+    return (int(p_min), int(q_min))
+
+
+def print_pq(angle: float, pq: tuple[int, int], name: str) -> None:
+    p, q = pq
+    r = (2 * np.pi - angle) * p / (angle * q)
+    r = max(r, 1 / r)
+    print(f"{name}: ({p}, {q}) -> {r:.4f}")
+
+    return None
+
+
+def main(n: int) -> None:
+    angles = np.linspace(0, np.pi, n + 2)[1:-1]
+    for angle in angles:
+        print(f"Angle = {angle}")
+        print_pq(angle, compute_pq_frac(angle, max_denominator=12), "frac")
+        print_pq(angle, compute_pq_min(angle, max_subdiv=16), " min")
 
 
 if __name__ == "__main__":
-    main(12)
+    main(13)
